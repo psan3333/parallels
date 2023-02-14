e5c77998b1c1efb750e260c3a193f0795bc2832d
psan3333 <59166366+psan3333@users.noreply.github.com>
2023-02-14 09:14:53 +0700

    Update code_GPU.c

--- code_GPU.c
+++ code_GPU.c
@@ -7,16 +7,16 @@
 #define angle (2 * PI / N);
 #define PGI_ACC_TIME 1
 
-void initialize_arr(double* sin_values) {
-    double inc = 0;
+void initialize_arr_gpu(double* sin_values) {
+    double angle_for_sin_calc = 0;
     #pragma acc parallel loop gang worker num_workers(4) vector_length(32)
     for (int i = 0; i < N; i++){
-        sin_values[i] = sin(inc);
-        inc += angle;
+        sin_values[i] = sin(angle_for_sin_calc);
+        angle_for_sin_calc += angle;
     }
 }
 
-double summ_vals(double* sin_values){
+double summ_vals_gpu(double* sin_values){
     double summ = 0.0f;
     #pragma acc parallel loop reduction(+:summ)
     for (int i = 0; i < N; i++) {
@@ -38,8 +38,8 @@ int main() {
     
     //подсчёт времени инициализации и подсчёта суммы элементов массива на GPU
     clock_t calc_init_start = clock();
-    initialize_arr(sin_values);
-    double summ = summ_vals(sin_values);
+    initialize_arr_gpu(sin_values);
+    double summ = summ_vals_gpu(sin_values);
     clock_t calc_init_end = clock();
     double calc_init_time = (double)(calc_init_end - calc_init_start) / CLOCKS_PER_SEC;
     printf("Sum 2: %lf; time: %lf\n", summ, calc_init_time);
