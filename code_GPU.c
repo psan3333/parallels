b3027cc990cdd976e2969f6c179698da8b6ed404
psan3333 <59166366+psan3333@users.noreply.github.com>
2023-02-17 17:51:47 +0700

    Update code_GPU.c

--- code_GPU.c
+++ code_GPU.c
@@ -7,20 +7,22 @@
 
 int main()
 {
-    double *sin_values = (double *)calloc(N, sizeof(double));
-    #pragma acc data create(sin_values[:N])
-
-    #pragma acc parallel loop
-    for (int i = 0; i < N; i++)
-    {
-        sin_values[i] = sin(2 * PI / N * i);
-    }
-    
+    double *sin_values = (double*)calloc(N, sizeof(double));
     double summ = 0.0f;
-    #pragma acc parallel loop reduction(+:summ)
-    for (int i = 0; i < N; i++)
+    #pragma acc data create(sin_values[:N]) copy(summ)
     {
-        summ += sin_values[i];
+        #pragma acc parallel loop
+        for (int i = 0; i < N; i++)
+        {
+            sin_values[i] = sin(2 * PI / N * i);
+        }
+        
+        #pragma acc parallel loop reduction(+:summ)
+        for (int i = 0; i < N; i++)
+        {
+            summ += sin_values[i];
+        }
+
     }
 
     printf("Sum: %lf\n", summ);
