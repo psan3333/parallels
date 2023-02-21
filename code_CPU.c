9b9f43f623a0fa4ab4cec2fe0c0784dfa182cb2b
psan3333 <59166366+psan3333@users.noreply.github.com>
2023-02-21 14:48:36 +0700

    Update code_CPU.c

--- code_CPU.c
+++ code_CPU.c
@@ -4,17 +4,14 @@
 #include <math.h>
 #define N 10000000
 #define PI 3.14159265358979323846
-#define angle (2 * PI / N);
 
 int main() {
 
-    //инициализация и подсчёт суммы элементов массива на CPU
-    double* sin_values = (double*)calloc(N, sizeof(double));
+    float* sin_values = (float*)calloc(N, sizeof(float));
     clock_t calc_init_start = clock();
-    double summ = 0.0f, calc_sin_angle = 0.0f;
+    float summ = 0.0f;
     for (int i = 0; i < N; i++) {
-        sin_values[i] = sin(calc_sin_angle);
-        calc_sin_angle += angle;
+        sin_values[i] = sinf(2 * PI * i / N);
         summ += sin_values[i];
     }
     clock_t calc_init_end = clock();
